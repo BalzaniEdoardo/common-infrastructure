d8254a203f6334712373048976aa417a7306ae7e
Horea Christian <chr@chymera.eu>
2023-10-12 09:56:20 -0400

    Added slides

---
+++ slides/compile.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+TARGET="${1}"
+WHITELIST="
+	article.tex
+	poster.tex
+	slides.tex
+	"
+
+if [ $TARGET = "all" ] || [ "$TARGET" == "" ]; then
+	for ITER_TARGET in *.tex; do
+		if [[ $WHITELIST =~ (^|[[:space:]])$ITER_TARGET($|[[:space:]]) ]];then
+			ITER_TARGET=${ITER_TARGET%".tex"}
+			./compile.sh ${ITER_TARGET}
+		fi
+	done
+else
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	#pythontex.py ${TARGET}.tex &&\
+	bibtex ${TARGET} &&\
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	pdflatex -shell-escape ${TARGET}.tex
+	#pdflatex -shell-escape ${TARGET}.tex &&\
+	#pythontex.py ${TARGET}.tex &&\
+	#pdflatex -shell-escape ${TARGET}.tex &&\
+	#bibtex ${TARGET} &&\
+	#pdflatex -shell-escape ${TARGET}.tex &&\
+	#pdflatex -shell-escape ${TARGET}.tex &&\
+	#pdflatex -shell-escape ${TARGET}.tex
+	#pdflatex -shell-escape ${TARGET}.tex
+fi
